33a4540684978ab39dc7fefe1eb19e284f001969
Knotmaxx <100964850+Knotmaxx@users.noreply.github.com>
2022-03-05 16:59:13 -0300

    Update teste_bot.py

--- teste_bot.py
+++ teste_bot.py
@@ -4,7 +4,6 @@ bot = telebot.TeleBot("5120413625:AAGmo0oeON97mHd_oZhmM9LrzlMm_Ch_h84")
 
 @bot.message_handler(func= lambda m : True)
 def responder(mensagem):
-	bot.send_message(mensagem.chat.id, "olá, o bot está funcionando!")
-
+      bot.send_message(mensagem.chat.id, "olá, o bot está funcionando!")
 
 bot.polling()
